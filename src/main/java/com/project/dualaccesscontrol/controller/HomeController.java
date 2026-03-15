16cba6f292dfaf537587cacb73ed92b5be1c53a9
nagendra061 <nagendravarma061@gmail.com>
2026-03-15 16:19:16 +0530

    minor changes on root file.

--- src/main/java/com/project/dualaccesscontrol/controller/HomeController.java
+++ src/main/java/com/project/dualaccesscontrol/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-    @GetMapping("/")
+    @GetMapping("/api")
     public String home() {
         return "Welcome to Dual Access Control System API. Please use /api/auth/login or /api/auth/register to get an access token.";
     }
